4ea2fda1997440633592c778363461a9d3f51177
Jack Vu <jacknvu98@gmail.com>
2024-02-14 16:34:05 -0500

    made some progress

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,17 +1,21 @@
 import { Box } from "@mui/material";
 import { orange } from "@mui/material/colors";
-import React from "react";
+import React, { useState } from "react";
 
 function Card({ data }) {
   const { id, imageUrl, isFlipped, isMatched } = data;
+  const [flippedCard, setFlippedCard] = useState(null);
   return (
     <Box
       width={100}
       height={100}
       bgcolor={"white"}
       sx={{ "&:hover": { bgcolor: orange[300] } }}
+      onClick={() => setFlippedCard(true)}
     >
-      <img src={imageUrl} alt={`card-${id}`} loading="lazy" height={"100%"} />
+      {flippedCard ? (
+        <img src={imageUrl} alt={`card-${id}`} loading="lazy" height={"100%"} />
+      ) : null}
     </Box>
   );
 }

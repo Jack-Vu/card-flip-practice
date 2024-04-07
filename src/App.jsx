71fc8f97862c21f45ccf7e35815ce249d5bb4c46
Jack Vu <jacknvu98@gmail.com>
2024-04-06 21:59:16 -0400

    started leaderboard

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { StartGameSetup } from "./components/StartGameSetup.jsx";
 import { Box } from "@mui/material";
 import { GameCompletion } from "./components/GameCompletion.jsx";
 import { WelcomeModal } from "./components/WelcomeModal.jsx";
+import { LeaderBoard } from "./components/LeaderBoard.jsx";
 
 function App() {
   const { gameStarted, gameCompleted } = useContext(CardDataContext);
@@ -15,9 +16,10 @@ function App() {
       <WelcomeModal />
       <Navbar />
       <Box id="main-container" display="flex" justifyContent="center" my={5}>
-        {gameStarted && <CardGrid />}
+        {/* {gameStarted && <CardGrid />}
         {gameCompleted && <GameCompletion />}
-        {!gameStarted && !gameCompleted && <StartGameSetup />}
+        {!gameStarted && !gameCompleted && <StartGameSetup />} */}
+        <LeaderBoard />
       </Box>
     </>
   );
